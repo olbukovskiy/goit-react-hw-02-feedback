48d7830413595f581a89430753f9d2d1add45145
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-11-19 23:59:34 +0200

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { FeedbackVidget } from "./FeedbackVidget/FeedbackVidget";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
---
+++ src/components/FeedbackVidget/FeedbackVidget.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class FeedbackVidget extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+    render() { return; }
+}
